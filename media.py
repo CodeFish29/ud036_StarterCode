ccaa3cadd773ac964a23947627e412e1dc941d2c
CodeFish29 <33793521+CodeFish29@users.noreply.github.com>
2017-11-18 17:11:21 -0700

    Add files via upload
    fixed pep-8 style issues

--- media.py
+++ media.py
@@ -5,6 +5,8 @@ Description: Stores all info about a video
 Attributes: title - name of video
             youtube_url - url to video
 """
+
+
 class Video():
     def __init__(self, title, video):
         self.title = title
@@ -16,6 +18,8 @@ Parent: Video
 Description: Stores all info about a movie
 Attributes: poster_image_url - url to movie poster image 
 """
+
+
 class Movie(Video):
     # This class provides a way to store movie related information
     def __init__(self, title, art, trailer):
